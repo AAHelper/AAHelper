cbfb970ec5c1219f06d7576b115dd0193bf2902f
Mike MacRae <mrfunyon@gmail.com>
2018-07-12 22:56:21 -0700

    Fixed up settings and added proper html to the index page

--- aafinder/forms.py
+++ aafinder/forms.py
@@ -1,8 +1,13 @@
 from django.forms import ModelForm
 from aafinder.models import Meeting
+from django.utils.translation import gettext_lazy as _
 
 
 class MeetingForm(ModelForm):
     class Meta:
         model = Meeting
         fields = ['area', 'codes', 'types']
+
+        labels = {
+            'types': _('Day'),
+        }
