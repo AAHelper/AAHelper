1790387999968c21964cab816224498c22d3e1ef
Mike MacRae <mrfunyon@gmail.com>
2018-12-29 14:53:09 -0800

    Added GDAL and GEOS library paths

--- aahelper/settings.py
+++ aahelper/settings.py
@@ -156,3 +156,5 @@ if ON_HEROKU:
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
